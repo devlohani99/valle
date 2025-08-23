5f75206c6d050b773190085edf8a75836dbc45ff
devlo90 <devlohani14251425@gmail.com>
2025-08-24 01:06:36 +0530

    6e2b_sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
+import {Sandbox} from "@e2b/code-interpreter";
+
 import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event,step }) => {
+    const sandboxId= await step.run("get-sandbox-id",async()=>{
+      const sandbox = await Sandbox.create(
+        "webgenie-devlo-2"
+      );
+      return sandbox.sandboxId;
+    });
+
+
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React Snippets.",
@@ -14,6 +25,12 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       `Write the following snippet: ${event.data.value}`
     );
+
+    const sandboxUrl=await step.run("get-sandbox-url",async()=>{
+      const sandbox = await getSandbox(sandboxId);
+      const host= sandbox.getHost(3000);
+      return `https://${host}`;
+    });
 
     return { output };
   }
